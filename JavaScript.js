3d414187891097651a43080e27731c7f67f60ef7
agentguzlo <87193053+agentguzlo@users.noreply.github.com>
2024-02-16 02:26:18 +0200

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -7,3 +7,38 @@ function toggleSelection(button) {
     // Додаємо клас "selected" до натисканої кнопки
     button.classList.add('selected');
 }
+function updateBudgetList() {
+    const budgetList = document.querySelector('.budgets');
+    const totalAmountElement = document.querySelector('.total-amount');
+    
+    // Очищаємо список перед оновленням
+    budgetList.innerHTML = '';
+
+    let totalAmount = 0;
+
+    // Зчитуємо дані з бази даних та оновлюємо список розходів
+    const transaction = db.transaction(['budget'], 'readonly');
+    const budgetStore = transaction.objectStore('budget');
+
+    const request = budgetStore.openCursor();
+
+    request.onsuccess = function (event) {
+        const cursor = event.target.result;
+
+        if (cursor) {
+            const listItem = document.createElement('li');
+            listItem.innerHTML = `<span class="category">${cursor.value.category}</span><span class="amount">${cursor.value.amount}</span>`;
+            budgetList.appendChild(listItem);
+
+            // Додаємо розхід до загальної суми
+            totalAmount += parseFloat(cursor.value.amount);
+
+            cursor.continue();
+        } else {
+            // Виводимо загальну суму після оновлення списку
+            if (totalAmountElement) {
+                totalAmountElement.textContent = `Загальна сума: ${totalAmount} грн`;
+            }
+        }
+    };
+}
